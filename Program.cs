08a5beebcee50ef00486f5ab57ddef62ceda2a3b
stenhaug1 <thomas.p26@proton.me>
2026-02-26 13:33:18 +0100

    readded the code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,33 @@
-﻿namespace week_2;
-
-class Program
+﻿DateTime Dato = DateTime.Now;
+Console.WriteLine("What would you like to be named?");
+string username = Console.ReadLine();
+Console.WriteLine("Do you have any hobbies?");
+string hobbies = Console.ReadLine();
+var hour = Dato.Hour;
+var output = $" {username}";
+if (hobbies == "No" || hobbies == "no")
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+
+}
+else
+{
+    output = output + " I hope you get to enjoy " + hobbies + " today :)";
+}
+
+Dictionary<string, string> greetings = new Dictionary<string, string>();
+greetings.Add("morning", "Good morning" + output);
+greetings.Add("afternoon", "Good afternoon" + output);
+greetings.Add("evening", "Good evening" + output);
+
+switch (hour)
+{
+    case < 12:
+        Console.WriteLine(greetings["morning"]);
+        break;
+    case < 18:
+        Console.WriteLine(greetings["afternoon"]);
+        break;
+    default:
+        Console.WriteLine(greetings["evening"]);
+        break;
 }
